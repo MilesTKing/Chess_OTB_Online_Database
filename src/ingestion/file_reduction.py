e8a59fbb86f9022e4de7a76e4bea30b3dd4850fa
Miles King <kingmilest@gmail.com>
2026-04-03 21:59:17 -0400

    Added spark and hdfs to pip requirements.

--- src/ingestion/file_reduction.py
+++ src/ingestion/file_reduction.py
@@ -1,13 +1,13 @@
 import chess.pgn as chess
 from pathlib import Path
 import time
-file_stats = Path('../../data/raw/reduced_lichess.pgn').stat()
+file_stats = Path('../../data/raw/lichess/reduced_lichess.pgn').stat()
 file_size = 0
 games_buffer = []
 start_time = time.perf_counter()
 max_file_size = 1000000000
 games_finished = False
-with open('../../data/raw/lichess.pgn') as lichess_file, open('../../data/raw/reduced_lichess.pgn', 'w') as outfile:
+with open('../../data/raw/lichess.pgn') as lichess_file, open('../../data/raw/lichess/reduced_lichess.pgn', 'w') as outfile:
         while file_size < max_file_size and not games_finished:
             for x in range(50000):
                 game = chess.read_game(lichess_file)
@@ -17,7 +17,7 @@ with open('../../data/raw/lichess.pgn') as lichess_file, open('../../data/raw/re
                 games_buffer.append(str(game))
             chunk = "".join(games_buffer)
             outfile.write(chunk)
-            file_size = Path('../../data/raw/reduced_lichess.pgn').stat().st_size
+            file_size = Path('../../data/raw/lichess/reduced_lichess.pgn').stat().st_size
             print(file_size)
             games_buffer.clear()
             print(f"Time: {time.perf_counter() - start_time} seconds")
