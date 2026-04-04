bc00db5f7633e8d7d3459ce90c1973a612eebb9c
Miles King <kingmilest@gmail.com>
2026-04-04 14:41:54 -0400

    Added OTB database to hadoop files.

--- src/ingestion/run_hdfs.py
+++ src/ingestion/run_hdfs.py
@@ -6,5 +6,5 @@ client = InsecureClient('http://localhost:9870', user='root')
 client.makedirs('/chess-data/raw/lichess')
 
 # Upload file
-client.upload('/chess-data/raw/lichess', '../../data/raw/lichess/reduced_lichess.pgn')
-
+client.upload('/chess-data/raw/lichess', '../../data/raw/lichess/reduced_lichess.pgn', overwrite=True)
+client.upload('/chess-data/raw/OTB_databases', '../../data/raw/OTB_databases/LumbrasGigaBase2025-06.db3', overwrite=True)
