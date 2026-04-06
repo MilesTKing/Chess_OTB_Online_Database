f10c7623930ca1713d38a90f71b2b86020583475
Miles King <kingmilest@gmail.com>
2026-04-06 01:40:00 -0400

    run_hdfs now uploads all files in raw, retrying upon failure.

--- src/ingestion/run_hdfs.py
+++ src/ingestion/run_hdfs.py
@@ -1,11 +1,57 @@
 from hdfs import InsecureClient
+from pathlib import Path
+import logging
+import time
 
-client = InsecureClient("http://namenode:9870", user="root")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("hdfs-upload")
 
-client.makedirs("/chess-data/raw/pgn", permission=755)
+hdfs_local_raw = Path("/app/data/raw")
+hdfs_root = "/chess-data/raw"
+max_retries = 5
 
-client.upload(
-    "/chess-data/raw/pgn",
-    "/app/data/raw/pgn/super_reduced_lichess.pgn",
-    overwrite=True
-)
+def get_hdfs_client():
+    for attempt in range(1, max_retries + 1):
+        try:
+            hdfs_client = InsecureClient("http://namenode:9870", user="root")
+            return hdfs_client
+        except Exception as e:
+            logger.error(f"Connection failed (attempt {attempt}): {e}")
+
+    logger.error("Failed to connect to HDFS after multiple attempts")
+    raise ConnectionError("Unable to connect to HDFS")
+
+
+# Uploads files
+def upload_all_files(client):
+    client.delete("/chess-data/raw/OTB_databases", recursive=True)
+    files = list(hdfs_local_raw.rglob("*.pgn"))
+
+    if not files:
+        logger.warning("No PGN files found")
+        return
+
+    for file in files:
+        relative_path = file.relative_to(hdfs_local_raw)
+        hdfs_path = f"{hdfs_root}/{relative_path.as_posix()}"
+        hdfs_dir = str(Path(hdfs_path).parent)
+
+        # ensure directory exists
+        try:
+            client.makedirs(hdfs_dir, permission=755)
+        except Exception as e:
+            logger.warning(f"Failed to create directory {hdfs_dir}: {e}")
+
+        # upload file to hdfs
+        for attempt in range(1, max_retries + 1):
+            try:
+                client.upload(
+                    hdfs_path,
+                    str(file),
+                    overwrite=False)
+                break
+            except Exception as e:
+                logger.warning(f"Uploading {file} failed: {e}")
+
+client = get_hdfs_client()
+upload_all_files(client)
